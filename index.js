ab998ba638647f352dff0dc27511e8aeef6e34e5
Emona Santiago <emonasantiago1207@gmail.com>
2022-05-18 11:21:44 +0200

    compressie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const { Router } = require('express');
 const express = require('express');
+const compression = require('compression')
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express(); 
 const url = "https://chipr.api.fdnd.nl/v1/project"
@@ -11,7 +12,9 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.set('');
 
-app.get("/", (request, response) => {
+app.use(compression())
+
+app.get("/", (_request, response) => {
      //1. Fetch projects from chippr API
      fetch(url)
      .then(response => response.json())
@@ -24,7 +27,7 @@ app.get("/", (request, response) => {
 
 });
 
-app.get("/:id", (request, response) => {
+app.get("/:id", (_request, response) => {
      fetch(url)
      .then(response => response.json())
      .then(projects => {
@@ -36,6 +39,12 @@ app.get("/:id", (request, response) => {
      .catch(err => console.log(err))
 
 });
+
+// cache-control would be to set the cache header
+app.use(function (_request, respond, next) {
+     respond.set('Cache-control', 'public, max-age=300')
+   })
+
 
 app.listen(process.env.PORT || 3000, () => console.log(`App avaiable on http://localhost:3000`)) 
 
