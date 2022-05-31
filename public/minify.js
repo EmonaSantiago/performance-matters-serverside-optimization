806033bd4171d6917fa2977b28b9321cff2f9350
Emona Santiago <emonasantiago1207@gmail.com>
2022-05-31 12:50:34 +0200

    got rid of some unnecessary content

--- public/minify.js
+++ public/minify.js
@@ -9,17 +9,18 @@ const minifyCssFile = 'public/css/style.min.css'
 // Wrapped in a function so we can use async/await
 fs.readFile(cssFile, async (err, data) => {
     const output = await 
+
 // We pass in an array of the plugins we want to use: `cssnano` and `autoprefixer`
+    // The `css` property of `output` is the minified CSS as a string
     postcss([cssnano, autoprefixer])
     .process(data)
-    // The `css` property of `output` is the minified CSS as a string
     .then((data) => {
         fs.writeFile(minifyCssFile, data, (err) => {
           if (err) console.log(err)
           console.log('Successfully written minified css to ' + minifyCssFile + '.')
         })
-      })
-  })
+    })
+})
   
 
 
